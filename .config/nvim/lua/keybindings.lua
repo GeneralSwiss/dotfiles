7c1f289a30a2e81af32014740afe635f25e39091
Nick <nicolas.farrier@gmail.com>
2022-05-09 22:57:06 -0700

    Update nvim config

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -1,4 +1,4 @@
-
+vim.g.mapleader = ' '
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true }
 keymap('n', '<c-j>', '<c-w>j', opts)
@@ -7,4 +7,4 @@ keymap('n', '<c-h>', '<c-w>h', opts)
 keymap('n', '<c-l>', '<c-w>l', opts)
 keymap('n', '<c-k>', '<c-w>k', opts)
 keymap('i', 'jk', '<esc>', opts)
-
+keymap('n', '<leader>f', [[:NvimTreeToggle<CR>]], {});
--- .config/nvim/lua/packages.lua
+++ .config/nvim/lua/packages.lua
@@ -3,8 +3,13 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
 Plug 'tpope/vim-sensible'
+Plug 'tpope/vim-surround'
 Plug 'rafi/awesome-vim-colorschemes'
 Plug 'neovim/nvim-lspconfig'
 Plug 'airblade/vim-rooter'
+Plug 'kyazdani42/nvim-web-devicons'
+Plug 'kyazdani42/nvim-tree.lua'
 
 vim.call('plug#end')
+
+require('nvim-tree').setup {}
